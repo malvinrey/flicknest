4d85de9b70f15f9956b919be8882e5ffab207c84
johanesrendy <johanesrendy9@gmail.com>
2024-11-21 20:16:54 +0700

    feat: add forgot-password page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'pages/login_page.dart';
 import 'pages/register_page.dart';
 import 'pages/home_page.dart';
 import 'pages/sign_in_screen.dart';
+import 'pages/forgot_password.dart';
 
 void main() {
+  // Aktifkan PathUrlStrategy untuk URL tanpa hash
+  setUrlStrategy(PathUrlStrategy());
   runApp(MyApp());
 }
 
@@ -12,16 +16,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App with Multiple Pages',
+      title: 'Flicknext',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(), // Halaman utama pertama
+      home: HomePage(),
       routes: {
         '/login': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
         '/sign-in': (context) => SignInScreen(),
-
+        '/forgot-password': (context) => ForgotPasswordPage(),
       },
     );
   }
